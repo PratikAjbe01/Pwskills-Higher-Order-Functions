01502469c2d2e2522b7a740a34eaf9f9d71d7739
Pratik Ajbe <124169308+PratikAjbe01@users.noreply.github.com>
2023-03-31 22:22:23 +0530

    Add files via upload

---
+++ problem5/script5.js
@@ -0,0 +1,3 @@
+let url="http://www.linledin.com";
+const chec1=new RegExp("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
+console.log(chec1.test(url));
